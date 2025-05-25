971d2bbb392943361e0db28ad5261b49fe4e22df
Ricardo Vale <ricardo_vale@live.com.pt>
2025-05-25 19:00:54 +0100

    Update meta.tsx

--- config/meta.tsx
+++ config/meta.tsx
@@ -1,50 +1,50 @@
 export const meta = {
   metadataBase: new URL('https://pinexio.vercel.app'),
-  title: 'Pinexio - Documentation template',
+  title: 'RJV TECHNOLOGIES LTD - Documentation',
   description:
-    'A customizable Open Source documentation template built with Next.js',
-  authors: [{ name: 'Sanjay Rajeev' }],
+    'documentation for RJV TECHNOLOGIES LTD',
+  authors: [{ name: 'Ricardo Jorge do Vale' }],
   keywords: [
-    'Pinexio',
-    'documentation template',
-    'template',
-    'Next.js',
-    'React',
-    'JavaScript',
+    'RJV TECHNOLOGIES LTD',
+    'documentation',
+    'computer science',
+    'AI',
+    'Physics',
+    'Mathematics',
   ],
-  publisher: 'Sanjay Rajeev',
-  creator: 'Sanjay Rajeev',
+  publisher: 'Ricardo Jorge do Vale',
+  creator: 'Ricardo Jorge do Vale',
   openGraph: {
     type: 'website',
-    title: 'Pinexio - Documentation Template',
+    title: 'RJV TECHNOLOGIES LTD - Documentation',
     description:
-      'A customizable open-source documentation template built with Next.js.',
+      'documentation for RJV TECHNOLOGIES LTD',
     images: [
       {
         url: '/og_image.png',
         width: 1200,
         height: 630,
-        alt: 'Pinexio Documentation Template',
+        alt: 'documentation for RJV TECHNOLOGIES LTD',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image', // Type of Twitter card
-    title: 'Pinexio - Documentation Template', // Twitter card title
+    title: 'RJV TECHNOLOGIES LTD - Documentation', // Twitter card title
     description:
-      'A customizable open-source documentation template built with Next.js.', // Twitter card description
+      'documentation for RJV TECHNOLOGIES LTD', // Twitter card description
     images: ['/og_image.png'], // Image used in the Twitter card
-    creator: '@sanjayrajeev', // Twitter handle of the content creator (optional)
+    creator: '@rjvtechnologies', // Twitter handle of the content creator (optional)
   },
   // SEO Enhancements
   alternates: {
-    canonical: 'https://pinexio.vercel.app', // Set the canonical URL
+    canonical: 'documentation-rho-two.vercel.app', // Set the canonical URL
   },
   robots: 'index, follow', // Allows search engines to index and follow links
   // Optional: Hreflang for multilingual content (if applicable)
   hreflang: {
-    en: 'https://pinexio.vercel.app', // English version URL
+    en: 'documentation-rho-two.vercel.app', // English version URL
     // Add more hreflang if you have other languages (example: Spanish)
-    // "es": "https://pinexio.vercel.app/es",
+    // "es": "documentation-rho-two.vercel.app/es",
   },
 };
